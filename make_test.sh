bc8d31115471a75e55a1efe9c6861b5c257f936b
Benjamin Resch <benjamin.resch@uni-tuebingen.de>
2020-09-29 22:34:39 +0200

    - More challenging test.

--- make_test.sh
+++ make_test.sh
@@ -7,7 +7,10 @@ dd if=/dev/urandom bs=1M count=100 of=filein
 ln -s filein linkin
 mkdir dirin
 mkdir dirfilledin
-mkdir dirfilledin/dir
+mkdir dirfilledin/dir1
+mkdir dirfilledin/dir1/dir2
+mkdir dirfilledin/dir1/dir2/dir3
+dd if=/dev/urandom bs=1M count=80 of=dirfilledin/dir1/dir2/dir3/test2
 touch dirfilledin/test1
 dd if=/dev/urandom bs=1K count=1 of=dirfilledin/test2
 dd if=/dev/urandom bs=1M count=1 of=dirfilledin/test3
